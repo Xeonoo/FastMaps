76194e7f3f9aa0b6f471f99ba8fad9e3e06f97d2
Maksymilian <ts3joker@gmail.com>
2022-07-26 16:42:12 +0200

    Initial Commit

---
+++ FastMaps/FastMapsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastMapsApp.swift
+//  FastMaps
+//
+//  Created by Maksymilian Patoń on 26/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct FastMapsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
